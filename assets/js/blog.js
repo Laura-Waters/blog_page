a6c33f2ad5297bcd6dc75ab28d48cbd318bcf9f9
Laura <laurawatersalston@gmail.com>
2024-06-05 20:21:56 -0400

    blog html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,24 @@
+// DEPENDENCIES
+const themeButtonEl = document.getElementById('theme-btn');
+const bodyEl = document.getElementById('body'); 
+const backButtonEl = document.getElementById('back'); 
 
+// DATA / STATE 
+
+
+// USER INTERACTIONS 
+themeButtonEl.addEventListener('click', function() {
+    let isDark =  true; 
+
+    if (isDark) {
+        bodyEl.setAttribute('style', 'background-color:#d9e9e8', 'color:#1a1a1a');
+        isDark = !isDark;
+      } else {
+        bodyEl.setAttribute('style', 'background-color:#1a1a1a', 'color:#d9e9e8');
+        isDark = !isDark;
+      }
+}); 
+
+backButtonEl.addEventListener('click', function() {
+    window.location.href = "index.html";
+})
